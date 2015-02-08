4550023076bb9ee77004014f40beebc263533d49
osaboy <osazuwa@gmail.com>
2015-02-07 19:28:04 -0800

    Insertion and printing of nodes in a linkedlist
    Added insertion methods
    - inset which takes a Node and add it to the front of the queue
    - add which takes data, creates and node and then adds it to the front
    of the queue
    Added helper methods to prints the content of a linked list.

--- LinkedList.rb
+++ LinkedList.rb
@@ -1,19 +1,58 @@
 # classes for linked list and node
+require 'pry'
 
 class Node
-
   attr_accessor :data
   attr_accessor :next
   
-  def initialize (data, next_node = nil)
+  def initialize(data, next_node = nil)
     @data = data
     @next = next_node
+  end
+
+  def to_s
+    puts "#{@data} -->"
   end
 
 end
 
 class LinkedList
+  include Enumerable
 
-  attr_accessor :linklist
+  attr_accessor :head
+
+  def initialize(head = nil)
+  
+  end
+
+  def each()   
+    item = @head
+    while(item)
+      yield item
+      item = item.next
+    end
+  end
+
+  # method for inserting a new node
+  def insert(node)
+    @head = Node.new(node,head)
+  end
+
+  # method for obtaining data, creating a node and adding the node to the linkedlist 
+  def add(data)
+    new_node = Node.new(data)
+    new_node.next = @head
+    @head = new_node
+  end
+
+  def recursiveprint(node)
+    puts node
+    recursiveprint(node.next) unless node.next == nil
+  end
+
+  def to_s
+    recursiveprint(@head)
+  end
 
 end
+
